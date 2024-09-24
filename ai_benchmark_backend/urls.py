0e1b648b9c47851bc03989630df7ec12cbdf7bf5
HyunwookJung0827 <as9804@berkeley.edu>
2024-09-23 23:24:21 -0700

    requirements.txt

--- ai_benchmark_backend/urls.py
+++ ai_benchmark_backend/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
+from app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Set the localhost:8000/ to the ReactView
+    path('', ReactView.as_view(), name="anything"),
 ]

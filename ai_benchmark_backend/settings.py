8293b528f98ca525b46c03e87441d5c3b10b6851
HyunwookJung0827 <as9804@berkeley.edu>
2024-09-24 00:47:35 -0700

    Add STATIC_ROOT setting

--- ai_benchmark_backend/settings.py
+++ ai_benchmark_backend/settings.py
@@ -125,7 +125,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+import os
+
+# Add this line to set the STATIC_ROOT
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Ensure you have these settings as well
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
